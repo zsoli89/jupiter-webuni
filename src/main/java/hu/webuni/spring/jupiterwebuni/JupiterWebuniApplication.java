a26437ff348191457e5834be03fe001fc22b6dc5
zsoli89 <jakabzsoli89@gmail.com>
2023-06-14 19:52:30 +0200

    init

---
+++ src/main/java/hu/webuni/spring/jupiterwebuni/JupiterWebuniApplication.java
@@ -0,0 +1,13 @@
+package hu.webuni.spring.jupiterwebuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupiterWebuniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JupiterWebuniApplication.class, args);
+    }
+
+}
