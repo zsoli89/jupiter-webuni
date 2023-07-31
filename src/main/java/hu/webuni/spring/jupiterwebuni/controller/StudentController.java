907144609c1579674bd20c46679249deb0893ee2
zsoli89 <jakabzsoli89@gmail.com>
2023-08-01 00:29:43 +0200

    feat: implement upload and download picture

--- src/main/java/hu/webuni/spring/jupiterwebuni/controller/StudentController.java
+++ src/main/java/hu/webuni/spring/jupiterwebuni/controller/StudentController.java
@@ -2,17 +2,25 @@ package hu.webuni.spring.jupiterwebuni.controller;
 
 import hu.webuni.jupiterwebuni.api.StudentControllerApi;
 import hu.webuni.jupiterwebuni.api.model.StudentDto;
+import hu.webuni.spring.jupiterwebuni.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/student")
+@RequiredArgsConstructor
 public class StudentController implements StudentControllerApi {
+
+    private final StudentService studentService;
 
     @Override
     public Optional<NativeWebRequest> getRequest() {
@@ -37,6 +45,21 @@ public class StudentController implements StudentControllerApi {
     @Override
     public ResponseEntity<StudentDto> findById1(Long id) {
         return StudentControllerApi.super.findById1(id);
+    }
+
+    @Override
+    public ResponseEntity<Resource> getProfilePicture(Integer id) {
+        return ResponseEntity.ok(studentService.getProfilePicture(id));
+    }
+
+    @Override
+    public ResponseEntity<Void> uploadProfilePicture(Integer id, MultipartFile content) {
+        try {
+            studentService.saveProfilePicture(id, content.getInputStream());
+            return ResponseEntity.ok().build();
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
--- src/main/java/hu/webuni/spring/jupiterwebuni/service/StudentService.java
+++ src/main/java/hu/webuni/spring/jupiterwebuni/service/StudentService.java
@@ -4,20 +4,30 @@ import hu.webuni.jupiterwebuni.api.model.StudentDto;
 import hu.webuni.spring.jupiterwebuni.model.student.Student;
 import hu.webuni.spring.jupiterwebuni.model.student.StudentMapper;
 import hu.webuni.spring.jupiterwebuni.repository.StudentRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 @Service
 public class StudentService {
@@ -26,6 +36,19 @@ public class StudentService {
     private final StudentRepository repository;
     private final StudentMapper mapper;
     private final CentralEducationService centralEducationService;
+
+    @Value("${jupiterwebuni.content.profilePics}")
+    private String profilePicsFolder;
+
+    @PostConstruct
+    public void init() {
+        try {
+            Files.createDirectories(Path.of(profilePicsFolder));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 
     public List<StudentDto> findAll() {
         List<StudentDto> studentDtoList = repository.findAll().stream().map(mapper::entityToDto).toList();
@@ -94,4 +117,25 @@ public class StudentService {
         });
     }
 
+    private Path getProfilePicPathForStudent(Integer id) {
+        return Paths.get(profilePicsFolder, id.toString() + ".jpg");
+    }
+
+    public Resource getProfilePicture(Integer studentId) {
+        FileSystemResource fileSystemResource = new FileSystemResource(getProfilePicPathForStudent(studentId));
+        if(!fileSystemResource.exists())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return fileSystemResource;
+    }
+
+    public void saveProfilePicture(Integer id, InputStream is) {
+        if(!repository.existsById(Long.valueOf(id)))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        try {
+            Files.copy(is, getProfilePicPathForStudent(id), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
